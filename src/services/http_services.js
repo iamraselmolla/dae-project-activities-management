14b64162d51c47f8d0a3df8d7338055870455bfe
iamraselmolla <raselmolla6336@gmail.com>
2024-02-06 23:41:30 +0600

    installed axios, used http_services and userServices file

---
+++ src/services/http_services.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+const http_create = axios.create({
+    baseURL: 'http://localhost:5000/',
+    //   headers: {
+    //     Authorization: `Bearer ${token}`,
+    //   },
+});
+
+// http_create.interceptors.request.use(
+//     (config) => {
+//         if (typeof window !== 'undefined') {
+//             const token = localStorage.getItem('token');
+//             const userId = localStorage.getItem('localid');
+//             if (token) {
+//                 config.headers.authorization = `Bearer ${token}`;
+//             }
+//             if (userId) {
+//                 config.headers['user-id'] = userId;
+//             }
+//         }
+//         return config;
+//     },
+//     (error) => {
+//         console.log(error)
+//         return Promise.reject(error);
+//     }
+// );
+
+const http = {
+    get: http_create.get,
+    post: http_create.post,
+    put: http_create.put,
+    delete: http_create.delete,
+};
+
+
+export default http;
