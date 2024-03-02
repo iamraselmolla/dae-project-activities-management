3862c0c109e4a8bbcb9e4d684992d4827d0a639d
iamraselmolla <raselmolla6336@gmail.com>
2024-03-02 22:37:55 +0600

    added all project route

--- src/components/pages/dashboard/admin/SingleUser.js
+++ src/components/pages/dashboard/admin/SingleUser.js
@@ -13,111 +13,119 @@ const SingleUser = ({ index, user }) => {
 
     return (
         <>
-            <UserTitle block={blockB} union={unionB} index={index} />
-            <div className="mt-3">
-                <Formik initialValues={initialValues} onSubmit={handleSubmit}>
-                    <Form>
-                        <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 grid-cols-1 gap-4">
-                            <div>
-                                <label htmlFor="block">ব্লকের নাম (ইংরেজী)</label>
-                                <Field
-                                    type="text"
-                                    id="block"
+            <div className="collapse">
+                <input type="checkbox" />
+                <div className="collapse-title text-xl font-medium">
+                    <UserTitle block={blockB} union={unionB} index={index} />
+                </div>
+                <div className="collapse-content">
+                    <div className="mt-3">
+                        <Formik initialValues={initialValues} onSubmit={handleSubmit}>
+                            <Form>
+                                <div className="grid lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 grid-cols-1 gap-4">
+                                    <div>
+                                        <label htmlFor="block">ব্লকের নাম (ইংরেজী)</label>
+                                        <Field
+                                            type="text"
+                                            id="block"
 
-                                    name="block"
-                                    className="input input-bordered w-full"
-                                    readOnly
-                                    disabled
-                                />
-                            </div>
+                                            name="block"
+                                            className="input input-bordered w-full"
+                                            readOnly
+                                            disabled
+                                        />
+                                    </div>
 
-                            <div>
-                                <label htmlFor="blockB">ব্লকের নাম (বাংলা)</label>
-                                <Field
-                                    type="text"
-                                    id="blockB"
-                                    name="blockB"
-                                    className="input input-bordered w-full"
-                                    readOnly
-                                    disabled
-                                />
-                            </div>
+                                    <div>
+                                        <label htmlFor="blockB">ব্লকের নাম (বাংলা)</label>
+                                        <Field
+                                            type="text"
+                                            id="blockB"
+                                            name="blockB"
+                                            className="input input-bordered w-full"
+                                            readOnly
+                                            disabled
+                                        />
+                                    </div>
 
-                            <div>
-                                <label htmlFor="union">ইউনিয়নের নাম (ইংরেজী)</label>
-                                <Field
-                                    type="text"
-                                    id="union"
-                                    name="union"
-                                    className="input input-bordered w-full"
-                                    readOnly
-                                    disabled
-                                />
-                            </div>
+                                    <div>
+                                        <label htmlFor="union">ইউনিয়নের নাম (ইংরেজী)</label>
+                                        <Field
+                                            type="text"
+                                            id="union"
+                                            name="union"
+                                            className="input input-bordered w-full"
+                                            readOnly
+                                            disabled
+                                        />
+                                    </div>
 
-                            <div>
-                                <label htmlFor="unionB">ইউনিয়নের নাম (বাংলা)</label>
-                                <Field
-                                    type="text"
-                                    id="unionB"
-                                    name="unionB"
-                                    className="input input-bordered w-full"
-                                    readOnly
-                                    disabled
-                                />
-                            </div>
-
-
+                                    <div>
+                                        <label htmlFor="unionB">ইউনিয়নের নাম (বাংলা)</label>
+                                        <Field
+                                            type="text"
+                                            id="unionB"
+                                            name="unionB"
+                                            className="input input-bordered w-full"
+                                            readOnly
+                                            disabled
+                                        />
+                                    </div>
 
 
-                            <div>
-                                <label htmlFor="username">ইউজার নাম</label>
-                                <Field
-                                    type="text"
-                                    id="username"
-                                    name="username"
-                                    className="input input-bordered w-full"
-                                    readOnly
-                                    disabled
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="password"> পাসওয়ার্ড</label>
-                                <Field
-                                    type="text"
-                                    id="password"
-                                    name="password"
-                                    className="input input-bordered w-full"
-                                />
-                            </div>
-                            <div>
-                                <label htmlFor="name">উপসহকারী কৃষি অফিসারের নাম</label>
-                                <Field
-                                    type="text"
-                                    id="name"
-                                    name="name"
-                                    className="input input-bordered w-full"
-                                />
-                            </div>
 
-                            <div>
-                                <label htmlFor="mobile">উপসহকারী কৃষি অফিসারের মোবাইল নং</label>
-                                <Field
-                                    type="text"
-                                    id="mobile"
-                                    name="mobile"
-                                    className="input input-bordered w-full"
-                                />
-                            </div>
 
-                        </div>
+                                    <div>
+                                        <label htmlFor="username">ইউজার নাম</label>
+                                        <Field
+                                            type="text"
+                                            id="username"
+                                            name="username"
+                                            className="input input-bordered w-full"
+                                            readOnly
+                                            disabled
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="password"> পাসওয়ার্ড</label>
+                                        <Field
+                                            type="text"
+                                            id="password"
+                                            name="password"
+                                            className="input input-bordered w-full"
+                                        />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="name">উপসহকারী কৃষি অফিসারের নাম</label>
+                                        <Field
+                                            type="text"
+                                            id="name"
+                                            name="name"
+                                            className="input input-bordered w-full"
+                                        />
+                                    </div>
 
-                        <button type="submit" className="btn btn-success text-white mt-4">
-                            {user?.blockB} ব্লকের ইউজার তথ্য আপডেট সম্পাদন করুন
-                        </button>
-                    </Form>
-                </Formik>
+                                    <div>
+                                        <label htmlFor="mobile">উপসহকারী কৃষি অফিসারের মোবাইল নং</label>
+                                        <Field
+                                            type="text"
+                                            id="mobile"
+                                            name="mobile"
+                                            className="input input-bordered w-full"
+                                        />
+                                    </div>
+
+                                </div>
+
+                                <button type="submit" className="btn btn-success text-white mt-4">
+                                    {user?.blockB} ব্লকের ইউজার তথ্য আপডেট সম্পাদন করুন
+                                </button>
+                            </Form>
+                        </Formik>
+                    </div>
+                </div>
             </div>
+
         </>
     );
 };
