5519f6e1a061aded33dfb5aa367eb6935623c256
iamraselmolla <raselmolla6336@gmail.com>
2024-03-12 23:01:02 +0600

    added componenet in single group meeting

--- src/components/pages/DaeGroupMeeting/DaeGroupMeeting.js
+++ src/components/pages/DaeGroupMeeting/DaeGroupMeeting.js
@@ -8,6 +8,20 @@ const DaeGroupMeeting = () => {
             <SectionTitle title={"সকল কৃষক গ্রুপ সভা"} />
             <div className="container px-4 md:px-0 grid md:grid-cols-2 lg:grid-cols-4 grid-cols-1 gap-6">
                 <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
+                <SingleDaeGroupMeetings />
             </div>
         </section>
     );
