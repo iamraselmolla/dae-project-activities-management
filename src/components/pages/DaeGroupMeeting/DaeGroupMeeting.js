3479a95f98285f0787ec2c86d082b6992d55ff89
iamraselmolla <raselmolla6336@gmail.com>
2024-03-12 22:55:50 +0600

    added single dae meeting componenet

--- src/components/pages/DaeGroupMeeting/DaeGroupMeeting.js
+++ src/components/pages/DaeGroupMeeting/DaeGroupMeeting.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import SectionTitle from '../../shared/SectionTitle';
+import SingleDaeGroupMeetings from './SingleDaeGroupMeetings';
 
 const DaeGroupMeeting = () => {
     return (
-        <div>
-
-        </div>
+        <section className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <SectionTitle title={"সকল কৃষক গ্রুপ সভা"} />
+            <div className="container px-4 md:px-0 grid md:grid-cols-2 lg:grid-cols-4 grid-cols-1 gap-6">
+                <SingleDaeGroupMeetings />
+            </div>
+        </section>
     );
 };
 
---
+++ src/components/pages/DaeGroupMeeting/SingleDaeGroupMeetings.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleDaeGroupMeetings = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default SingleDaeGroupMeetings;
