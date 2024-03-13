38c72f80f4a9289d214cd4d60b08506f539d47af
iamraselmolla <raselmolla6336@gmail.com>
2024-03-13 12:41:06 +0600

    added get all groups api

--- src/components/pages/DaeGroupMeeting/DaeGroupMeeting.js
+++ src/components/pages/DaeGroupMeeting/DaeGroupMeeting.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import SectionTitle from '../../shared/SectionTitle';
-import SingleDaeGroupMeetings from './SingleDaeGroupMeetings';
+import React from "react";
+import SectionTitle from "../../shared/SectionTitle";
+import SingleDaeGroupMeetings from "./SingleDaeGroupMeetings";
 
 const DaeGroupMeeting = () => {
-    return (
-        <section className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <SectionTitle title={"সকল কৃষক গ্রুপ সভা"} />
-            <div className="container px-4 md:px-0 grid md:grid-cols-2 lg:grid-cols-4 grid-cols-1 gap-6">
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-                <SingleDaeGroupMeetings />
-            </div>
-        </section>
-    );
+  return (
+    <section className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+      <SectionTitle title={"সকল কৃষক গ্রুপ সভা"} />
+      <div className="container px-4 md:px-0 grid md:grid-cols-2 lg:grid-cols-4 grid-cols-1 gap-6">
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+        <SingleDaeGroupMeetings />
+      </div>
+    </section>
+  );
 };
 
 export default DaeGroupMeeting;
