2085b52d97f5b2f9333ba408ed8727dabfde25e9
iamraselmolla <raselmolla6336@gmail.com>
2024-03-26 23:36:53 +0600

    added delete confirmation

--- src/components/pages/training/SingleTraining.js
+++ src/components/pages/training/SingleTraining.js
@@ -32,6 +32,11 @@ const SingleTraining = ({ data }) => {
       }
     }
   }, [images]);
+  const handleTrainingDelete = () => {
+    if (window.confirm(`আপনি কি ${projectInfo?.short} প্রকল্পের ${toBengaliNumber(date?.startDate)} তারিখের ${subject} শিরোনামের প্রশিক্ষণ ডিলিট করতে চান?`)) {
+
+    }
+  }
 
   return (
     <div className="rounded-lg border pb-12 relative shadow-xl">
@@ -44,7 +49,7 @@ const SingleTraining = ({ data }) => {
         </div>
         <div className="flex items-center absolute top-3 right-0">
           <p className="px-2 py-1 flex gap-2 items-center bg-black text-white rounded-l-md ">
-            <BsCalendarDate /> <div>{toBengaliNumber(date.startDate)}</div>
+            <BsCalendarDate /> <div>{toBengaliNumber(date?.startDate)}</div>
           </p>
         </div>
       </div>
@@ -55,27 +60,27 @@ const SingleTraining = ({ data }) => {
         </div>
         <div>
           <div className="flex items-center gap-2">
-            <GiFarmer />{" "}
+            <GiFarmer />
             <p>
-              {" "}
-              {toBengaliNumber(farmers?.male + farmers?.female)} জন (পুরুষ{" "}
-              {toBengaliNumber(farmers?.male)} জন, মহিলা{" "}
+
+              {toBengaliNumber(farmers?.male + farmers?.female)} জন (পুরুষ
+              {toBengaliNumber(farmers?.male)} জন, মহিলা
               {toBengaliNumber(farmers?.female)} জন)
             </p>
           </div>
           <div className="flex items-center gap-2">
-            <BsFillCloudSunFill />{" "}
+            <BsFillCloudSunFill />
             <p>
               {season}/{fiscalYear}
             </p>
           </div>
           <div className="flex items-center gap-2">
-            <img src="images/project.png" alt="Project Icon" />{" "}
+            <img src="images/project.png" alt="Project Icon" />
             <p>{projectInfo?.details}</p>
           </div>
           <div className="flex absolute bottom-0 left-0 mt-3 w-full">
             <button className="bg-green-400 flex justify-center items-center w-full py-2 px-4 text-white font-bold">এডিট করুন </button>
-            <button className="bg-red-400 w-full justify-center items-center py-2 px-4 text-white font-bold">ডিলিট করুন</button>
+            <button onClick={handleTrainingDelete} className="bg-red-400 w-full justify-center items-center py-2 px-4 text-white font-bold">ডিলিট করুন</button>
           </div>
         </div>
       </div>
