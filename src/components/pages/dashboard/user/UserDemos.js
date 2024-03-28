271e61821b864a13bd4286a69b7040f6f6eea795
iamraselmolla <raselmolla6336@gmail.com>
2024-03-28 13:18:16 +0600

    added user field day table

--- src/components/pages/dashboard/user/UserDemos.js
+++ src/components/pages/dashboard/user/UserDemos.js
@@ -3,7 +3,7 @@ import React from "react";
 const UserDemos = () => {
   return (
     <div className="flex flex-col">
-      <div className="-m-1.5 overflow-x-auto">
+      <div className="mt-10 overflow-x-auto">
         <div className="p-1.5 min-w-full inline-block align-middle">
           <div className="border rounded-lg shadow overflow-hidden dark:border-gray-700 dark:shadow-gray-900">
             <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
