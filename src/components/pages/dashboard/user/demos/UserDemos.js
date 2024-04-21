bf31dfe55fc17a5084bcb799ea52a220550da5c1
iamraselmolla <raselmolla6336@gmail.com>
2024-04-21 11:33:39 +0600

    added delete confirm message of demo

--- src/components/pages/dashboard/user/demos/UserDemos.js
+++ src/components/pages/dashboard/user/demos/UserDemos.js
@@ -7,53 +7,56 @@ import UserSingleDemoTableRow from "./UserSingleDemoTableRow";
 import Loader from "../../../../shared/Loader";
 
 const UserDemos = () => {
-  const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   const [userDemos, setUserDemos] = useState([]);
-  const [loading, setLoading] = useState(false)
-  const [fetchEnd, setFetchEnd] = useState(false)
+  const [loading, setLoading] = useState(false);
+  const [fetchEnd, setFetchEnd] = useState(false);
   useEffect(() => {
     const fetchUserDemos = async () => {
-      setLoading(true)
+      setLoading(true);
       try {
-        setLoading(true)
+        setLoading(true);
         const result = await getUserDemos();
         if (result?.status === 200) {
           setUserDemos(result?.data?.data);
-          setLoading(false)
-          setFetchEnd(true)
+          setLoading(false);
+          setFetchEnd(true);
         }
+      } catch (err) {
+        toast.error(
+          "ইউজারের যুক্ত করা প্রদর্শনীর তথ্য আনতে সমস্যা হচ্ছে। দয়া করে সংশ্লিষ্ট কর্তৃপক্ষকে অবহিত করুন।"
+        );
+        setLoading(false);
+        setFetchEnd(true);
       }
-      catch (err) {
-        toast.error("ইউজারের যুক্ত করা প্রদর্শনীর তথ্য আনতে সমস্যা হচ্ছে। দয়া করে সংশ্লিষ্ট কর্তৃপক্ষকে অবহিত করুন।")
-        setLoading(false)
-        setFetchEnd(true)
-      }
-
-    }
+    };
     if (navigator.onLine) {
-      fetchUserDemos()
-    }
-    else {
-      makeSureOnline()
+      fetchUserDemos();
+    } else {
+      makeSureOnline();
     }
   }, [user]);
 
-  const handleDemoDeleting = async () => {
+  const handleDemoDeleting = async (project, date, demoInfo, farmersInfo) => {
     try {
-
+      if (
+        window.confirm(
+          `আপনি কি ${project} প্রকল্পের ${date?.season}/${date.fiscalYear} মৌসুমের ${demoInfo?.tech} প্রযুক্তির ${demoInfo?.crop} প্রদর্শনীপ্রাপ্ত ${farmersInfo?.name} কৃষকের তথ্য মুছে ফেলতে চান ?`
+        )
+      ) {
+      }
+    } catch (err) {
+      toast.error();
+      console.log(err);
     }
-    catch (err) {
-      toast.error()
-      console.log(err)
-    }
-  }
+  };
   return (
     <div className="flex flex-col">
       <div className="mt-10 overflow-x-auto">
         <div className="p-1.5 min-w-full inline-block align-middle">
           <div className="border rounded-lg shadow overflow-hidden dark:border-gray-700 dark:shadow-gray-900">
-            {!loading && fetchEnd && userDemos?.length > 0 &&
-              < table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+            {!loading && fetchEnd && userDemos?.length > 0 && (
+              <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                 <thead>
                   <tr className="divide-x font-extrabold divide-gray-200 dark:divide-gray-700">
                     <th
@@ -96,7 +99,8 @@ const UserDemos = () => {
                       scope="col"
                       className="py-4 whitespace-nowrap font-extrabold px-2  text-black text-center uppercase"
                     >
-                      মোবাইল, এনআইডি,<br /> কৃষি কার্ড, বিআইডি
+                      মোবাইল, এনআইডি,
+                      <br /> কৃষি কার্ড, বিআইডি
                     </th>
                     <th
                       scope="col"
@@ -137,25 +141,36 @@ const UserDemos = () => {
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 dark:divide-gray-700">
-                  {!loading && fetchEnd && userDemos?.length > 0 && userDemos?.map((single, index) => <UserSingleDemoTableRow handleDemoDeleting={handleDemoDeleting} data={single} index={index} />)}
+                  {!loading &&
+                    fetchEnd &&
+                    userDemos?.length > 0 &&
+                    userDemos?.map((single, index) => (
+                      <UserSingleDemoTableRow
+                        handleDemoDeleting={handleDemoDeleting}
+                        data={single}
+                        index={index}
+                      />
+                    ))}
                 </tbody>
               </table>
-            }
-            {!loading && fetchEnd && userDemos?.length < 1 && <h2 className="text-red-600 text-center text-2xl  font-extrabold">
-              কোনো প্রদর্শনীর তথ্য পাওয়া যায়নি!!
-            </h2>}
-            {loading && !fetchEnd &&
+            )}
+            {!loading && fetchEnd && userDemos?.length < 1 && (
+              <h2 className="text-red-600 text-center text-2xl  font-extrabold">
+                কোনো প্রদর্শনীর তথ্য পাওয়া যায়নি!!
+              </h2>
+            )}
+            {loading && !fetchEnd && (
               <div className="fixed daeLoader">
                 <Loader />
                 <h2 className="text-green-600 mt-3 text-4xl">
                   তথ্য আনা হচ্ছে। দয়া করে অপেক্ষা করুন
                 </h2>
-              </div>}
-
+              </div>
+            )}
           </div>
         </div>
       </div>
-    </div >
+    </div>
   );
 };
 
--- src/components/pages/dashboard/user/demos/UserSingleDemoTableRow.js
+++ src/components/pages/dashboard/user/demos/UserSingleDemoTableRow.js
@@ -1,104 +1,121 @@
-import React, { useEffect } from 'react';
-import { AiOutlineFileDone } from 'react-icons/ai';
-import { CiEdit } from 'react-icons/ci';
-import { MdOutlineDelete } from 'react-icons/md';
+import React, { useEffect } from "react";
+import { AiOutlineFileDone } from "react-icons/ai";
+import { CiEdit } from "react-icons/ci";
+import { MdOutlineDelete } from "react-icons/md";
 import { toBengaliNumber } from "bengali-number";
 import ImageGallery from "react-image-gallery";
-import formatDateToday from '../../../../utilis/formatDate';
+import formatDateToday from "../../../../utilis/formatDate";
 
 const UserSingleDemoTableRow = ({ data, index, handleDemoDeleting }) => {
-    const {
-        projectInfo,
-        demoTime,
-        farmersInfo,
-        address,
-        SAAO,
-        numbersInfo,
-        demoInfo,
-        demoDate,
-        production,
-        comment,
-        demoImages,
-        username
-    } = data;
-    const imagesArr = [];
-    useEffect(() => {
-        if (demoImages?.length > 0) {
-            for (const image of demoImages) {
-                imagesArr.push({ original: image, thumbnail: image });
-            }
-        }
-    }, [demoImages, username]);
-    return (
-        <tr className="divide-x divide-gray-200 dark:divide-gray-700">
-            <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
-                {toBengaliNumber(index + 1)}
-            </td>
-            <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
-                {projectInfo?.short}
-            </td>
-            <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
-                প্রযুক্তিঃ {demoInfo?.tech} <br />
-                ফসলঃ {demoInfo?.crop} <br />
-                জাতঃ {demoInfo?.variety} <br />
-            </td>
-            <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
-                {demoTime?.season} <br />
-                {demoTime?.fiscalYear}
-            </td>
-            <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
-                {farmersInfo?.name}, <br />
-                {farmersInfo?.fatherOrHusbandName}
-            </td>
-            <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
-                {address?.village}
-            </td>
-            <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
-                {numbersInfo?.mobile}, {numbersInfo?.NID}<br />
-                {numbersInfo?.agriCard}, {numbersInfo?.BID}
-            </td>
-            <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
-                রোপণঃ {formatDateToday(demoDate?.bopon)} <br />
-                বপণঃ {formatDateToday(demoDate?.ropon)} <br />
-                কর্তনঃ {formatDateToday(demoDate?.korton?.startDate)} - <br /> {formatDateToday(demoDate?.korton?.endDate)}
-            </td>
-            <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
-                ফলন/হেঃ {toBengaliNumber(production?.productionPerHector)} <br />
-                উৎপাদনঃ {toBengaliNumber(production?.totalProduction)} <br />
-                কন্ট্রোল প্লটঃ {toBengaliNumber(production?.sidePlotProduction)} <br />
-            </td>
-            <td className="text-center text-balance dashboard-image-control text-sm font-medium text-gray-800 dark:text-gray-200 p-2">
-                {demoImages[0].image ? <ImageGallery
-                    showFullscreenButton={true}
-                    showPlayButton={false}
-                    showNav={false}
-                    showThumbnails={false}
-                    autoPlay={true}
-                    items={imagesArr}
-                /> : 'No IMG'}
-            </td>
-            <td className="text-center text-balance dashboard-image-control text-sm font-medium text-gray-800 dark:text-gray-200 p-2 whitespace-nowrap">
-                কৃষকঃ  {comment?.farmersReview} <br />
-                {comment?.overallComment && 'অন্যান্যঃ ' + comment?.overallComment}
-            </td>
-            <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
-                {SAAO?.name} <br />
-                {toBengaliNumber(SAAO?.mobile)}
-            </td>
+  const {
+    projectInfo,
+    demoTime,
+    farmersInfo,
+    address,
+    SAAO,
+    numbersInfo,
+    demoInfo,
+    demoDate,
+    production,
+    comment,
+    demoImages,
+    username,
+  } = data;
+  const imagesArr = [];
+  useEffect(() => {
+    if (demoImages?.length > 0) {
+      for (const image of demoImages) {
+        imagesArr.push({ original: image, thumbnail: image });
+      }
+    }
+  }, [demoImages, username]);
+  return (
+    <tr className="divide-x divide-gray-200 dark:divide-gray-700">
+      <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
+        {toBengaliNumber(index + 1)}
+      </td>
+      <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
+        {projectInfo?.short}
+      </td>
+      <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
+        প্রযুক্তিঃ {demoInfo?.tech} <br />
+        ফসলঃ {demoInfo?.crop} <br />
+        জাতঃ {demoInfo?.variety} <br />
+      </td>
+      <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
+        {demoTime?.season} <br />
+        {demoTime?.fiscalYear}
+      </td>
+      <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
+        {farmersInfo?.name}, <br />
+        {farmersInfo?.fatherOrHusbandName}
+      </td>
+      <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
+        {address?.village}
+      </td>
+      <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
+        {numbersInfo?.mobile}, {numbersInfo?.NID}
+        <br />
+        {numbersInfo?.agriCard}, {numbersInfo?.BID}
+      </td>
+      <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
+        রোপণঃ {formatDateToday(demoDate?.bopon)} <br />
+        বপণঃ {formatDateToday(demoDate?.ropon)} <br />
+        কর্তনঃ {formatDateToday(demoDate?.korton?.startDate)} - <br />{" "}
+        {formatDateToday(demoDate?.korton?.endDate)}
+      </td>
+      <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
+        ফলন/হেঃ {toBengaliNumber(production?.productionPerHector)} <br />
+        উৎপাদনঃ {toBengaliNumber(production?.totalProduction)} <br />
+        কন্ট্রোল প্লটঃ {toBengaliNumber(production?.sidePlotProduction)} <br />
+      </td>
+      <td className="text-center text-balance dashboard-image-control text-sm font-medium text-gray-800 dark:text-gray-200 p-2">
+        {demoImages[0].image ? (
+          <ImageGallery
+            showFullscreenButton={true}
+            showPlayButton={false}
+            showNav={false}
+            showThumbnails={false}
+            autoPlay={true}
+            items={imagesArr}
+          />
+        ) : (
+          "No IMG"
+        )}
+      </td>
+      <td className="text-center text-balance dashboard-image-control text-sm font-medium text-gray-800 dark:text-gray-200 p-2 whitespace-nowrap">
+        কৃষকঃ {comment?.farmersReview} <br />
+        {comment?.overallComment && "অন্যান্যঃ " + comment?.overallComment}
+      </td>
+      <td className="dark:text-gray-200 font-medium p-2 text-center text-gray-800 text-sm whitespace-nowrap">
+        {SAAO?.name} <br />
+        {toBengaliNumber(SAAO?.mobile)}
+      </td>
 
-            <td className="p-3 flex gap-1 text-center whitespace-nowrap text-sm font-medium">
-                <div className="cursor-pointer">
-                    <AiOutlineFileDone size={35} color="green" />
-                </div>
-                <div className="cursor-pointer">
-                    <MdOutlineDelete size={35} color="red" />
-                </div>
-                <div className="cursor-pointer">
-                    <CiEdit size={35} color="black" />
-                </div>
-            </td>
-        </tr >
-    );
+      <td className="p-3 flex gap-1 text-center whitespace-nowrap text-sm font-medium">
+        <div className="cursor-pointer">
+          <AiOutlineFileDone size={35} color="green" />
+        </div>
+        <div className="cursor-pointer">
+          <MdOutlineDelete
+            onClick={() =>
+              handleDemoDeleting(
+                projectInfo?.short,
+                demoTime,
+                demoInfo,
+                farmersInfo
+              )
+            }
+            size={35}
+            color="red"
+          />
+        </div>
+        <div className="cursor-pointer">
+          <CiEdit size={35} color="black" />
+        </div>
+      </td>
+    </tr>
+  );
 };
 
 export default UserSingleDemoTableRow;
