47933979dab53e24da9d6a33ab90c2ea5fa65ec0
iamraselmolla <raselmolla6336@gmail.com>
2024-05-09 22:06:51 +0600

    edited the iitail loading state

--- src/components/AuthContext/AuthProvider.js
+++ src/components/AuthContext/AuthProvider.js
@@ -4,12 +4,11 @@ import React, { createContext, useEffect, useState } from "react";
 export const AuthContext = createContext();
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
-  const [initialLoading, setInitialLoading] = useState(false);
+  const [initialLoading, setInitialLoading] = useState(true);
   const [jwtToken, setJwtToken] = useState("");
   const [role, setRole] = useState(null);
   const [username, setUsername] = useState(null);
   useEffect(() => {
-    setInitialLoading(true);
     const storedUser = localStorage.getItem("CurrentUser");
     const storedUserToken = localStorage.getItem("CurrentUserToken");
     if (storedUser) {
