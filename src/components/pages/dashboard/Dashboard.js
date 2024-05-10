69bd77ea9eb8d9919fdd1daaa1c1ccf582e41329
iamraselmolla <raselmolla6336@gmail.com>
2024-05-10 11:19:58 +0600

    added useeffect on dashboard page based on role

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,8 +1,12 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AuthContext } from '../../AuthContext/AuthProvider';
 
 
 const Dashboard = () => {
+    const { user, role, username } = useContext(AuthContext);
+    useEffect(() => {
+
+    }, [user, role, username])
     return (
         <div>
             Dashboard
