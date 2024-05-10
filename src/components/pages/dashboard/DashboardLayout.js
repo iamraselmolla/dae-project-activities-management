a9528eb3ccadead0d97cbba9948f7a0ecbe53119
iamraselmolla <raselmolla6336@gmail.com>
2024-05-10 11:05:09 +0600

    edited the dashboard style

--- src/components/pages/dashboard/DashboardLayout.js
+++ src/components/pages/dashboard/DashboardLayout.js
@@ -7,7 +7,7 @@ const DashboardLayout = () => {
     <section className="grid py-0">
       <div className="grid relative grid-cols-5">
         <div className="col-span-1 mx-4 sticky top-0 h-screen">
-          <div className="py-5">
+          <div className="py-5 px-6">
             <DashboardMenu />
           </div>
         </div>
