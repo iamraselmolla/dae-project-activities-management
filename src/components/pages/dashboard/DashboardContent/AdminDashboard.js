b0ffbfc82c456168f101673d705fdc606250d3ca
iamraselmolla <raselmolla6336@gmail.com>
2024-05-15 22:22:53 +0600

    added bar chart for motivational tour

--- src/components/pages/dashboard/DashboardContent/AdminDashboard.js
+++ src/components/pages/dashboard/DashboardContent/AdminDashboard.js
@@ -17,9 +17,11 @@ import { FaUser } from "react-icons/fa";
 import { GiDiscussion } from "react-icons/gi";
 import { GrCheckboxSelected } from "react-icons/gr";
 import { AiOutlineFileDone } from "react-icons/ai";
+import { MdTour } from "react-icons/md";
+
 
 const AdminDashboard = () => {
-  const { projects, users, trainings, notes } = useSelector(
+  const { projects, users, trainings, notes, tours } = useSelector(
     (state) => state.dae
   );
   const [noteTypes, setNoteTypes] = useState([]);
@@ -99,17 +101,101 @@ const AdminDashboard = () => {
       text: "সম্পন্ন নোটস",
       backgroundColor: "#c4ff8b80",
     },
+    {
+      icon: <MdTour />,
+      count: tours?.length,
+      text: "উদ্বুদ্ধকরণ ভ্রমণ",
+      backgroundColor: "#c4ff8b80",
+    },
+    {
+      icon: <GoNote />,
+      count: notes?.length - completedNotes,
+      text: "অসম্পন্ন নোটস",
+      backgroundColor: "#a9cded59",
+    },
+    {
+      icon: <FaUser />,
+      count: users?.length - 1,
+      text: "ইউজার",
+      backgroundColor: "#dcfce7",
+    },
+
+
     // Add more cards as needed
   ];
-
+  const data5 = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
   return (
     <section className="py-5">
-      <div className="grid grid-cols-2 gap-5 justify-center items-center">
-        <div className="grid bg-white p-4 rounded-xl grid-cols-3 gap-3">
+      <div className="grid grid-cols-3 gap-5 justify-center items-center">
+        <div className="grid bg-white p-4 col-span-2 rounded-xl grid-cols-3 gap-3">
           {cards.map((card, index) => (
             <DashboardCard key={index} {...card} />
           ))}
         </div>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={data5}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+            barSize={20}
+          >
+            <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <CartesianGrid strokeDasharray="3 3" />
+            <Bar dataKey="pv" fill="#8884d8" background={{ fill: '#eee' }} />
+          </BarChart>
+        </ResponsiveContainer>
       </div>
       <div className="h-80 mt-8">
         <ResponsiveContainer
