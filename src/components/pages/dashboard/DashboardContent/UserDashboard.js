f125a18b0ac17de20bf85342b4c8fe30415b943f
iamraselmolla <raselmolla6336@gmail.com>
2024-05-19 13:06:51 +0600

    added user statics of activity

--- src/components/pages/dashboard/DashboardContent/UserDashboard.js
+++ src/components/pages/dashboard/DashboardContent/UserDashboard.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React from "react";
+import { GiDiscussion } from "react-icons/gi";
+import { useSelector } from "react-redux";
+import { FaBowlRice } from "react-icons/fa6";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { GiFarmer } from "react-icons/gi";
+import { FcReadingEbook } from "react-icons/fc";
+import { GoNote } from "react-icons/go";
+import { AiOutlineFileDone } from "react-icons/ai";
+import DashboardCard from "../../../shared/DashboardCard";
 
 const UserDashboard = () => {
-    return (
-        <div>
-            
+  const {
+    notes,
+    userData: { demos, fieldDays, schools, daeMeetins },
+  } = useSelector((state) => state.dae);
+  const completedNotes = notes?.filter((single) => single.completed).length;
+  const cards = [
+    {
+      icon: <FaBowlRice />,
+      count: demos?.length,
+      text: "প্রদর্শনী",
+      backgroundColor: "#ffe2e6", // Light pink
+    },
+    {
+      icon: <FaPeopleGroup />,
+      count: fieldDays?.length,
+      text: "মাঠ দিবস",
+      backgroundColor: "#fff4de", // Light yellow
+    },
+    {
+      icon: <GiFarmer />,
+      count: daeMeetins?.length,
+      text: "কৃষক সভা",
+      backgroundColor: "#dcfce7", // Light green
+    },
+    {
+      icon: <FcReadingEbook />,
+      count: schools?.length,
+      text: "স্কুল",
+      backgroundColor: "#f4e8ff", // Light purple
+    },
+    {
+      icon: <GoNote />,
+      count: notes?.length - completedNotes,
+      text: "অসম্পন্ন নোটস",
+      backgroundColor: "#a9cded", // Light blue
+    },
+    {
+      icon: <AiOutlineFileDone />,
+      count: completedNotes,
+      text: "সম্পন্ন নোটস",
+      backgroundColor: "#c4ff8b", // Light green
+    },
+  ];
+  return (
+    <section className="py-5">
+      <div className="grid grid-cols-3 gap-5 justify-center items-center">
+        <div className="grid bg-white p-4 col-span-2 rounded-xl grid-cols-3 gap-3">
+          {cards.map((card, index) => (
+            <DashboardCard key={index} {...card} />
+          ))}
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default UserDashboard;
--- src/components/store/projectSlice.js
+++ src/components/store/projectSlice.js
@@ -17,7 +17,6 @@ const initialState = {
     fieldDays: [],
     schools: [],
     daeMeetins: [],
-    schools: [],
   },
 };
 
