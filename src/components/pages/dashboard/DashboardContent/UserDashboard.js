d34879d47c18326a7d7ce99a75172e1089ad8eec
iamraselmolla <raselmolla6336@gmail.com>
2024-05-20 00:37:29 +0600

    added user demo data statics to the dashboard

--- src/components/pages/dashboard/DashboardContent/UserDashboard.js
+++ src/components/pages/dashboard/DashboardContent/UserDashboard.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { GiDiscussion } from "react-icons/gi";
+import React, { PureComponent } from 'react';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import { useSelector } from "react-redux";
 import { FaBowlRice } from "react-icons/fa6";
 import { FaPeopleGroup } from "react-icons/fa6";
@@ -8,12 +8,15 @@ import { FcReadingEbook } from "react-icons/fc";
 import { GoNote } from "react-icons/go";
 import { AiOutlineFileDone } from "react-icons/ai";
 import DashboardCard from "../../../shared/DashboardCard";
+import getFiscalYear from '../../../shared/commonDataStores';
+import { toBengaliNumber } from 'bengali-number';
 
 const UserDashboard = () => {
   const {
     notes,
     userData: { demos, fieldDays, schools, daeMeetins },
   } = useSelector((state) => state.dae);
+  const getRunningFiscalYear = toBengaliNumber(getFiscalYear());
   const completedNotes = notes?.filter((single) => single.completed).length;
   const cards = [
     {
@@ -53,6 +56,36 @@ const UserDashboard = () => {
       backgroundColor: "#c4ff8b", // Light green
     },
   ];
+
+  const allRunningYearDemo = demos?.filter(single => single?.demoTime?.fiscalYear === getRunningFiscalYear);
+  const kharip1 = allRunningYearDemo?.filter(single => single?.demoTime?.season === 'খরিপ-১');
+  const kharip2 = allRunningYearDemo?.filter(single => single?.demoTime?.season === 'খরিপ-২');
+  const robi = allRunningYearDemo?.filter(single => single?.demoTime?.season === 'রবি');
+  console.log(allRunningYearDemo)
+
+
+  const data = [
+    {
+      name: 'খরিপ-২',
+      pv: kharip2?.filter(single => !single?.completed).length,
+      uv: kharip2?.filter(single => single?.completed).length,
+      amt: 2400,
+    },
+    {
+      name: 'রবি',
+      uv: robi?.filter(single => single?.completed).length,
+      pv: robi?.filter(single => !single?.completed).length,
+      amt: 2210,
+    },
+    {
+      name: 'খরিপ-১',
+      uv: kharip1?.filter(single => single?.completed).length,
+      pv: kharip1?.filter(single => !single?.completed).length,
+      amt: 2400,
+    }
+  ];
+
+  console.log(data)
   return (
     <section className="py-5">
       <div className="grid grid-cols-3 gap-5 justify-center items-center">
@@ -61,6 +94,27 @@ const UserDashboard = () => {
             <DashboardCard key={index} {...card} />
           ))}
         </div>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
+            width={500}
+            height={300}
+            data={data}
+            margin={{
+              top: 20,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" stackId="a" fill="#8884d8" />
+            <Bar dataKey="uv" stackId="a" fill="#00ca92" />
+          </BarChart>
+        </ResponsiveContainer>
       </div>
     </section>
   );
