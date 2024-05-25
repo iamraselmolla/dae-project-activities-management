49c58d9693b68a879bad616a2f4377bcda404ad2
iamraselmolla <raselmolla6336@gmail.com>
2024-05-25 22:57:38 +0600

    added distribution linechart data

--- src/components/pages/dashboard/DashboardContent/AdminDashboard.js
+++ src/components/pages/dashboard/DashboardContent/AdminDashboard.js
@@ -126,57 +126,14 @@ const AdminDashboard = () => {
 
     }
   });
-  const data6 = [
-    {
-      name: '18-24',
-      uv: 31.47,
-      pv: 2400,
-      fill: '#8884d8',
-    },
-    {
-      name: '25-29',
-      uv: 26.69,
-      pv: 4567,
-      fill: '#83a6ed',
-    },
-    {
-      name: '30-34',
-      uv: 15.69,
-      pv: 1398,
-      fill: '#8dd1e1',
-    },
-    // {
-    //   name: '35-39',
-    //   uv: 8.22,
-    //   pv: 9800,
-    //   fill: '#82ca9d',
-    // },
-    // {
-    //   name: '40-49',
-    //   uv: 8.63,
-    //   pv: 3908,
-    //   fill: '#a4de6c',
-    // },
-    // {
-    //   name: '50+',
-    //   uv: 2.63,
-    //   pv: 4800,
-    //   fill: '#d0ed57',
-    // },
-    // {
-    //   name: 'unknow',
-    //   uv: 6.67,
-    //   pv: 4800,
-    //   fill: '#ffc658',
-    // },
-  ];
+  const allProjectsDistributionsDataArray = seasonsArr.map(single => {
+    return {
+      name: single,
+      pv: distributions?.filter(singleDistribution => singleDistribution?.time.season === single && singleDistribution?.time.fiscalYear === runningFiscalYear).length,
 
-  const style = {
-    top: '50%',
-    right: 0,
-    transform: 'translate(0, -50%)',
-    lineHeight: '24px',
-  };
+    }
+  });
+
 
 
   return (
@@ -217,7 +174,7 @@ const AdminDashboard = () => {
               <AreaChart
                 width={500}
                 height={200}
-                data={data6}
+                data={allProjectsDistributionsDataArray}
                 syncId="anyId"
                 margin={{
                   top: 10,
