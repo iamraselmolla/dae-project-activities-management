b4a78dc04ffcef181b1cda1f1b68d36f73ddff80
iamraselmolla <raselmolla6336@gmail.com>
2024-05-29 23:50:44 +0600

    edited the not found page style

--- src/components/pages/allprojects/AllProjects.js
+++ src/components/pages/allprojects/AllProjects.js
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const AllProjects = () => {
+    const [activeProjects, setActiveProjects] = useState([]);
+    const [completedProjects, setCompletedProjects] = useState([]);
+    const { projects } = useSelector(state => state.dae)
+
+    useEffect(() => {
+        const active = projects.filter((project) => !project.end);
+        const completed = projects.filter((project) => project.end);
+        setActiveProjects(active);
+        setCompletedProjects(completed);
+    }, [projects])
     return (
         <div>
 
