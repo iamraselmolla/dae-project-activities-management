1a62707a1c9da6c6e50604883265cdf240eea925
iamraselmolla <raselmolla6336@gmail.com>
2024-05-29 16:05:13 +0600

    added all team data into the section

--- src/components/pages/home/DevelopmentTeam.js
+++ src/components/pages/home/DevelopmentTeam.js
@@ -1,10 +1,26 @@
 import React from "react";
+import SingleMember from "./SingleMember";
 
 function DevelopmentTeam() {
   const teamData = [
     {
       name: "শেখ সাখাওয়াত হোসেন",
       title: "UAO & Project Advisor",
+      role: "UAO & Project Advisor",
+      img: "images/uao.png",
+      socialLinks: {
+        facebook: "",
+        github: "",
+        linkedin: "",
+        whatsapp: "+",
+        portfolio: "",
+        gmail: "sakhoat.dae@gmailcom",
+      },
+    },
+    {
+      name: "মোঃ রাসেল মোল্লা",
+      title: "Full Stack Developer",
+      role: "Project Manager & Lead",
       img: "https://avatars.githubusercontent.com/u/108364128?v=4",
       socialLinks: {
         facebook: "iamraselmolla",
@@ -16,9 +32,10 @@ function DevelopmentTeam() {
       },
     },
     {
-      name: "মোঃ রাসেল মোল্লা",
-      title: "Project Manager and Project Lead",
-      img: "https://avatars.githubusercontent.com/u/108364128?v=4",
+      name: "শেখ মুহম্মদ যোবায়ের হোসাইন",
+      title: "Software Engineer",
+      role: "Co-Ordinator & Team Lead",
+      img: "https://avatars.githubusercontent.com/u/94280579?v=4",
       socialLinks: {
         facebook: "iamraselmolla",
         github: "iamraselmolla",
@@ -26,6 +43,20 @@ function DevelopmentTeam() {
         whatsapp: "+8801944835365",
         portfolio: "http://iamraselmolla.netlify.app/",
         gmail: "raselmolla6336@gmail.com",
+      },
+    },
+    {
+      name: "হাবিবুল্লাহ হাওলাদার সুমন",
+      title: "Software Engineer",
+      role: "Database architect",
+      img: "https://media.licdn.com/dms/image/D5603AQG_zDgeX1-UKQ/profile-displayphoto-shrink_800_800/0/1711819018065?e=1722470400&v=beta&t=2wWdTSURK1XeH5lBlIBRUhWHIHQt2ZDpOPtlpMY4Z6E",
+      socialLinks: {
+        facebook: "",
+        github: "",
+        linkedin: "habibullah-hs",
+        whatsapp: "",
+        portfolio: "",
+        gmail: "habibullah.diu@gmail.com",
       },
     },
   ];
@@ -33,127 +64,12 @@ function DevelopmentTeam() {
     <section className={"mx-auto max-w-7xl px-2 sm:px-6 lg:px-8"}>
       <div class=" relative">
         <div class="grid grid-cols-1 pb-4 text-center">
-          <h3 class="mb-6 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">
+          <h3 class="mb-2 md:text-3xl text-2xl md:leading-normal leading-normal font-semibold">
             ডেভেলপমেন্ট টিমের পরিচিতি
           </h3>
         </div>
         <div class="grid md:grid-cols-2 lg:grid-cols-4 grid-cols-1 items-center mt-8 gap-8">
-          <div>
-            <div class="team p-6 rounded-md shadow-md dark:shadow-gray-800 dark:border-gray-700 bg-white dark:bg-slate-900 relative">
-              <div class="bg-indigo-600/10 dark:bg-indigo-600/30 absolute inset-0 rounded-md -mt-[10px] -ms-[10px] w-[98%] h-[98%] -z-1"></div>
-              <img
-                src="https://avatars.githubusercontent.com/u/108364128?v=4"
-                class="w-24 h-24 rounded-full shadow-md dark:shadow-gray-800"
-                alt="Ronny Jofra"
-              />
-              <div class="content mt-4">
-                মোঃ রাসেল মোল্লা
-                <span class="text-slate-400 block">
-                  Project Manager and Lead
-                </span>
-                <p class="text-slate-400 mt-4">
-                  If the distribution of letters and 'words' is random
-                </p>
-                <ul class="list-none mt-4 space-x-1 flex">
-                  <li>
-                    <a
-                      class="w-8 h-8 inline-flex items-center justify-center transition duration-500 ease-in-out text-base text-center border border-gray-100 dark:border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 hover:text-white"
-                      href="/page-team"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="w-4 h-4"
-                      >
-                        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-                      </svg>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="w-8 h-8 inline-flex items-center justify-center transition duration-500 ease-in-out text-base text-center border border-gray-100 dark:border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 hover:text-white"
-                      href="/page-team"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="w-4 h-4"
-                      >
-                        <rect
-                          x="2"
-                          y="2"
-                          width="20"
-                          height="20"
-                          rx="5"
-                          ry="5"
-                        ></rect>
-                        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-                        <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-                      </svg>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="w-8 h-8 inline-flex items-center justify-center transition duration-500 ease-in-out text-base text-center border border-gray-100 dark:border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 hover:text-white"
-                      href="/page-team"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="w-4 h-4"
-                      >
-                        <path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path>
-                      </svg>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      class="w-8 h-8 inline-flex items-center justify-center transition duration-500 ease-in-out text-base text-center border border-gray-100 dark:border-gray-800 rounded-md hover:border-indigo-600 dark:hover:border-indigo-600 hover:bg-indigo-600 hover:text-white"
-                      href="/page-team"
-                    >
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                        fill="none"
-                        stroke="currentColor"
-                        stroke-width="2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        class="w-4 h-4"
-                      >
-                        <path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path>
-                        <rect x="2" y="9" width="4" height="12"></rect>
-                        <circle cx="4" cy="4" r="2"></circle>
-                      </svg>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          {teamData.map((data, index) => <SingleMember data={data} key={data?.socialLinks?.gmail} />)}
         </div>
       </div>
     </section>
