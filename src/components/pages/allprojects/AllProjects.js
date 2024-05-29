edc8bbed54d9ec7c96861be69e1a935d8c335bd9
iamraselmolla <raselmolla6336@gmail.com>
2024-05-30 00:25:57 +0600

    added loading spinner

--- src/components/pages/allprojects/AllProjects.js
+++ src/components/pages/allprojects/AllProjects.js
@@ -4,20 +4,25 @@ import SectionTitle from '../../shared/SectionTitle';
 import SingleProject from './SingleProject';
 import { findAllProjectsData } from '../../../services/userServices';
 import toast from 'react-hot-toast';
+import Loader from '../../shared/Loader';
 
 
 const AllProjects = () => {
     const [projects, setAllProjects] = useState([])
+    const [loading, setLoading] = useState(true)
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const result = await findAllProjectsData();
                 if (result?.status === 200) {
                     setAllProjects(result?.data?.data)
+                    setLoading(false)
                 }
             }
             catch (err) {
                 toast.error("প্রকল্পের তথ্য সার্ভার থেকে আনতে অসুবিধা হচ্ছে।")
+            } finally {
+                setLoading(false)
             }
         }
         fetchData()
@@ -27,11 +32,14 @@ const AllProjects = () => {
     return (
         <section className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
             <div className="container">
-                <SectionTitle title={'সকল প্রকল্প'} />
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
-                    {projects?.map((single, index) => <SingleProject key={single?.name?.details} single={single} />)}
+                {!loading ? <>
+                    <SectionTitle title={'সকল প্রকল্প'} />
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5">
+                        {projects?.map((single, index) => <SingleProject key={single?.name?.details} single={single} />)}
 
-                </div>
+                    </div></> : <>
+                    <Loader />
+                </>}
             </div>
         </section>
     );
