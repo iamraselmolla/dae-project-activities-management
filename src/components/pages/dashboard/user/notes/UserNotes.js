7763c388f992c58e92e8a8cb17f7b1aba7e51861
iamraselmolla <raselmolla6336@gmail.com>
2024-06-03 23:55:38 +0600

    added completed notes number in the title

--- src/components/pages/dashboard/user/notes/UserNotes.js
+++ src/components/pages/dashboard/user/notes/UserNotes.js
@@ -68,7 +68,7 @@ const UserNotes = () => {
       <div className="mt-10 overflow-x-auto">
         <div className="p-1.5 min-w-full inline-block align-middle">
           <AddModuleButton link={"add-note"} btnText={"নোট যুক্ত করুন"} />
-          <SectionTitle title={"অসম্পন্ন নোট"} />
+          <SectionTitle title={`অসম্পন্ন নোট ${toBengaliNumber(completedNotes?.length)}`} />
           <div className="border rounded-lg shadow overflow-hidden dark:border-gray-700 dark:shadow-gray-900 mb-16">
             <>
               <table className="min-w-full divide-y bg-white  divide-gray-200 dark:divide-gray-700">
