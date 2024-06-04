8c1d3434e83d72af074b4d5bc885d897dd537be8
iamraselmolla <raselmolla6336@gmail.com>
2024-06-04 16:10:38 +0600

    added dashboard responsiveness in the menu icon and text

--- src/components/shared/DashboardMenuItem.js
+++ src/components/shared/DashboardMenuItem.js
@@ -9,8 +9,8 @@ const DashboardMenuItem = ({ link, text, icon }) => {
                 isPending
                     ? "block px-1 lg:w-full md:w-full w-auto py-4 bg-slate-900"
                     : isActive
-                        ? "rounded-xl px-2 md:px-2 my-2 lg:w-full md:w-full w-auto py-2 flex items-center gap-2 md:gap-4 ps-0 md:ps-4 lg:ps-6  font-bold  bg-blue-200"
-                        : "rounded-xl px-1 my-2 lg:w-full md:w-full w-auto py-2 md:py-4 lg:py-4 flex items-center gap-2 md:gap-4 ps-0 md:ps-4 lg:ps-6 font-bold "
+                        ? "rounded-xl px-2 md:px-2 my-2 lg:w-full md:w-full w-auto py-2 md:py-4 flex items-center gap-2 md:gap-4 ps-0 md:ps-4 lg:ps-6  font-bold  bg-blue-200"
+                        : "rounded-xl px-1 my-2 lg:w-full md:w-full w-auto py-2 md:py-4  lg:py-4 flex items-center gap-2 md:gap-4 ps-0 md:ps-4 lg:ps-6 font-bold "
             }
         >
             {icon} {text}
