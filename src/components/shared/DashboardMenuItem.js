e1213e743ba19b0737280939200dcbb4cbd2a39a
iamraselmolla <raselmolla6336@gmail.com>
2024-06-04 16:50:20 +0600

    added justify control to the dashboard

--- src/components/shared/DashboardMenuItem.js
+++ src/components/shared/DashboardMenuItem.js
@@ -9,7 +9,7 @@ const DashboardMenuItem = ({ link, text, icon }) => {
                 isPending
                     ? "block px-3 md:px-4 lg:w-full md:w-full w-auto py-4 bg-slate-900"
                     : isActive
-                        ? "rounded-xl px-3 md:px-2 my-2 lg:w-full md:w-full w-auto py-2 md:py-4 flex items-center gap-2 md:gap-4 ps-0 justify-center md:ps-4 lg:ps-6  font-bold  bg-blue-200"
+                        ? "rounded-xl px-3 md:px-2 my-2 lg:w-full md:w-full w-auto py-2 md:py-4 flex items-center gap-2 md:gap-4 ps-0 lg:justify-start md:justify-start justify-center md:ps-4 lg:ps-6  font-bold  bg-blue-200"
                         : "rounded-xl px-1 my-2 lg:w-full md:w-full w-auto py-2 md:py-4  lg:py-4 flex items-center gap-2 md:gap-4 ps-0 md:ps-4 lg:ps-6 font-bold "
             }
         >
