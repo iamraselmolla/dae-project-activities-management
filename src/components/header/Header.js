ed6b70910cd383889fea9b760c40979f4cd8272c
iamraselmolla <raselmolla6336@gmail.com>
2024-06-04 23:40:48 +0600

    edited the user profile image to icon

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,6 +4,8 @@ import { AiOutlineLogin } from "react-icons/ai";
 import Login from "../shared/Login";
 import { AuthContext } from "../AuthContext/AuthProvider";
 import HeaderMenuItem from "../shared/HeaderMenuItem";
+import { FaRegCircleUser } from "react-icons/fa6";
+
 
 const Header = () => {
   const [showMenu, setShowMenu] = useState(false);
@@ -95,11 +97,7 @@ const Header = () => {
                     >
                       <span className="absolute -inset-1.5"></span>
                       <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
+                      <FaRegCircleUser size={40} color="white" />
                     </button>
                   </div>
 
