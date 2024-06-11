771b858789e0934b3c0e349abacdcd8bc6a369b4
iamraselmolla <raselmolla6336@gmail.com>
2024-06-11 22:52:37 +0600

    changed the why title

--- src/components/pages/home/IconBoxSection.js
+++ src/components/pages/home/IconBoxSection.js
@@ -54,7 +54,7 @@ const IconBoxSection = () => {
   return (
     <section className="py-16 bg-white">
       <div className="text-center mb-8">
-        <h2 className="text-xl text-green-400 font-extrabold mb-4">কি কি সেবা যুক্ত আছে?</h2>
+        <h2 className="text-xl text-green-600 font-extrabold mb-4">কি কি সেবা যুক্ত আছে?</h2>
         <h3 className="text-3xl font-semibold mb-8">নিম্নোক্ত সেবাসমূহ ছাড়াও আরও রয়েছে</h3>
       </div>
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
