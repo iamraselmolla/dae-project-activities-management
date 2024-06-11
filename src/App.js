7fef4f5cb1d3a45706df1fbdc2a95581c2abccd5
iamraselmolla <raselmolla6336@gmail.com>
2024-06-11 22:49:31 +0600

    added custom class varaible

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
+import "../src/css/custom_css.css";
 function App() {
   return (
     <div className="App">
