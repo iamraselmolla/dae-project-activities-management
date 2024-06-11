09102fe4f2dfa63d2e059af2c3ed7a194528a34e
iamraselmolla <raselmolla6336@gmail.com>
2024-06-12 00:56:50 +0600

    edited the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,19 +4,24 @@ import FooterMenuItem from "../shared/FooterMenuItem";
 const Footer = () => {
   return (
     <footer className="theme-bg py-6">
-      <div className="mx-auto flex flex-col items-center text-center max-w-7xl px-2 sm:px-6 lg:px-8">
-        <img src="/images/logo.png" width={100} alt="" srcSet="" />
-        <div>
-          <div className="flex flex-wrap justify-center mt-6 gap-4">
-            <FooterMenuItem link="" text="হোম" />
-            <FooterMenuItem link="demos" text="প্রদর্শনী" />
-            <FooterMenuItem link="trainings" text="প্রশিক্ষণ" />
-            <FooterMenuItem link="fielddays" text="মাঠদিবস" />
-            <FooterMenuItem link="distributions" text="উপকরণ বিতরণ" />
-            <FooterMenuItem link="dae-group-meeting" text="ডিএই কৃষক গ্রুপ সভা" />
-            <FooterMenuItem link="motivational-tour" text="উদ্বুদ্ধকরণ ভ্রমণ" />
-            <FooterMenuItem link="all-schools" text="স্কুল" />
-          </div>
+      <div className="mx-auto flex items-center justify-around text-center max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div><img src="/images/logo.png" width={100} alt="" srcSet="" /></div>
+
+        <div className="flex flex-col gap-4 justify-center">
+          <FooterMenuItem link="" text="হোম" />
+          <FooterMenuItem link="demos" text="প্রদর্শনী" />
+        </div>
+        <div className="flex flex-col gap-4 justify-center">
+          <FooterMenuItem link="trainings" text="প্রশিক্ষণ" />
+          <FooterMenuItem link="fielddays" text="মাঠদিবস" />
+        </div>
+        <div className="flex flex-col gap-4 justify-center">
+          <FooterMenuItem link="distributions" text="উপকরণ বিতরণ" />
+          <FooterMenuItem link="dae-group-meeting" text="ডিএই কৃষক গ্রুপ সভা" />
+        </div>
+        <div className="flex flex-col gap-4 justify-center">
+          <FooterMenuItem link="motivational-tour" text="উদ্বুদ্ধকরণ ভ্রমণ" />
+          <FooterMenuItem link="all-schools" text="স্কুল" />
         </div>
       </div>
     </footer>
@@ -24,3 +29,6 @@ const Footer = () => {
 };
 
 export default Footer;
+
+
+
