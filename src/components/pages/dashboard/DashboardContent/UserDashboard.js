a31904a55f457f29ff167e5e9f0a2a899c2c6f52
iamraselmolla <raselmolla6336@gmail.com>
2024-06-29 00:31:37 +0600

    edited the dashboard responsivesness

--- src/components/pages/dashboard/DashboardContent/UserDashboard.js
+++ src/components/pages/dashboard/DashboardContent/UserDashboard.js
@@ -28,8 +28,7 @@ import { seasonsArr } from '../../../shared/MessageConst';
 
 const UserDashboard = () => {
   const {
-    notes,
-    demos, fieldDays, schools, daeMeetings,
+    notes, demos, fieldDays, schools, daeMeetings,
   } = useSelector((state) => state.dae);
   const [noteTypes, setNoteTypes] = useState([]);
   const [Notedata, setData] = useState([]);
@@ -158,7 +157,7 @@ const UserDashboard = () => {
   });
   return (
     <section className="py-5">
-      <div className="grid  grid-cols-1 md:grid-cols-3 gap-5 justify-center items-center">
+      <div className="grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 justify-center items-center">
         <div className="grid bg-white px-4 py-6 col-span-2 rounded-xl md:grid-cols-2 grid-cols-2 lg:grid-cols-3 gap-3">
           {cards.map((card, index) => (
             <DashboardCard key={index} {...card} />
