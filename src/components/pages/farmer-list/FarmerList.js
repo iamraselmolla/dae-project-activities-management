f5c0b21ca0d050e088e0f1466252f0f0edb78833
iamraselmolla <raselmolla6336@gmail.com>
2024-07-05 14:14:23 +0600

    displayed the farmer fata to the list table

--- src/components/pages/farmer-list/FarmerList.js
+++ src/components/pages/farmer-list/FarmerList.js
@@ -4,6 +4,7 @@ import AddModuleButton from '../../shared/AddModuleButton';
 import { getAllFarmers } from '../../../services/userServices';
 import toast from 'react-hot-toast';
 import { AuthContext } from '../../AuthContext/AuthProvider';
+import { tobengableNumber } from "bengali-number"
 import LoaderWithDynamicMessage from '../../shared/LoaderWithDynamicMessage';
 
 const FarmerList = () => {
@@ -31,44 +32,48 @@ const FarmerList = () => {
             <div className="mt-3">
                 <SectionTitle title={'সংরক্ষণকৃত কৃষকের তথ্য'} />
             </div>
-            <div className="overflow-x-auto">
-                <table className="table table-xs">
-                    <thead className="font-bold text-black text-xl">
-                        <tr>
-                            <th></th>
-                            <th>কৃষকের নাম</th>
-                            <th> পিতার নাম</th>
-                            <th>মোবাইল, NID</th>
-                            <th>BID, কৃষি কার্ড</th>
-                            <th>ঠিকানা</th>
-                            <th>মন্তব্য</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th>1</th>
-                            <td>Cy Ganderton</td>
-                            <td>Quality Control Specialist</td>
-                            <td>Littel, Schaden and Vandervort</td>
-                            <td>Canada</td>
-                            <td>12/16/2020</td>
-                            <td>Blue</td>
-                        </tr>
+            {!loading && allFarmers?.length > 0 &&
+                <div className="overflow-x-auto">
+                    <table className="table table-xs">
+                        <thead className="font-bold text-black text-xl">
+                            <tr>
+                                <th></th>
+                                <th>কৃষকের নাম</th>
+                                <th> পিতার নাম</th>
+                                <th>মোবাইল, NID</th>
+                                <th>BID, কৃষি কার্ড</th>
+                                <th>ঠিকানা</th>
+                                <th>মন্তব্য</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {allFarmers?.map((farmer, index) => (
+                                <tr key={farmer?._id}>
+                                    <th>{tobengableNumber(index + 1)}</th>
+                                    <td>{farmer?.name}</td>
+                                    <td>{farmer?.fathersOrHusbandsName}</td>
+                                    <td>{farmer?.numer.mobile}</td>
+                                    {/* <td>{farmer?.bid}</td>
+                                    <td>{farmer?.address}</td>
+                                    <td>{farmer?.note}</td> */}
+                                </tr>
+                            ))}
 
-                    </tbody>
-                    <tfoot className="font-bold text-black text-xl">
-                        <tr>
-                            <th></th>
-                            <th>কৃষকের নাম</th>
-                            <th> পিতার নাম</th>
-                            <th>মোবাইল, NID</th>
-                            <th>BID, কৃষি কার্ড</th>
-                            <th>ঠিকানা</th>
-                            <th>মন্তব্য</th>
-                        </tr>
-                    </tfoot>
-                </table>
-            </div>
+                        </tbody>
+                        <tfoot className="font-bold text-black text-xl">
+                            <tr>
+                                <th></th>
+                                <th>কৃষকের নাম</th>
+                                <th> পিতার নাম</th>
+                                <th>মোবাইল, NID</th>
+                                <th>BID, কৃষি কার্ড</th>
+                                <th>ঠিকানা</th>
+                                <th>মন্তব্য</th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            }
             <AddModuleButton link={"add-farmer"} btnText={"নতুন কৃষকের তথ্য সংরক্ষণ করুন।"} />
             {loading && <LoaderWithDynamicMessage />}
         </section>
