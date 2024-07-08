d4902a78582e785c423ac20e64ec89e7653a549c
iamraselmolla <raselmolla6336@gmail.com>
2024-07-08 23:16:06 +0600

    added fixed button

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import Header from './header/Header';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import Footer from './footer/Footer';
+import { GoPlusCircle } from 'react-icons/go';
+import { IoIosCloseCircleOutline } from "react-icons/io";
+
 
 const Layout = () => {
     return (
@@ -9,6 +12,23 @@ const Layout = () => {
             <Header></Header>
             <div className="bg-slate-100">
                 <Outlet></Outlet>
+                <div className="fixed bottom-5 right-5">
+
+                    <div className="flex justify-center items-center flex-col gap-3">
+                        <Link to="/addDemo">প্রদর্শনী</Link>
+                        <Link to="/addDemo">প্রদর্শনী</Link>
+                        <Link to="/addDemo">প্রদর্শনী</Link>
+                        <Link to="/addDemo">প্রদর্শনী</Link>
+                        <Link to="/addDemo">প্রদর্শনী</Link>
+                        <Link to="/addDemo">প্রদর্শনী</Link>
+                        <Link to="/addDemo">প্রদর্শনী</Link>
+                    </div>
+
+
+                    <GoPlusCircle color="green" size={40} className="mt-4" />
+                    <IoIosCloseCircleOutline color="green" size={45} className="mt-4" />
+
+                </div>
             </div>
             <Footer></Footer>
         </main>
