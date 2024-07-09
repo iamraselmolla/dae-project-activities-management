d7173e3a3c6861bb12130285fc798d8de168199e
iamraselmolla <raselmolla6336@gmail.com>
2024-07-09 12:04:51 +0600

    fixed the user notes options

--- src/components/shared/FixedMenu.js
+++ src/components/shared/FixedMenu.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { GoPlusCircle } from "react-icons/go";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthContext/AuthProvider";
 
 const FixedMenu = () => {
     const [open, setOpen] = useState(false);
+    const {role} = useContext(AuthContext)
 
 
 
@@ -11,13 +13,21 @@ const FixedMenu = () => {
         <>
              <div className="fixed flex justify-center flex-col bottom-5 right-5 items-center">
                     <div className={`menu-drawer ${open ? 'open' : ''}`}>
+                     {role === 'user' &&   <>
                         <Link className='menu-item' to="/addDemo">প্রদর্শনী</Link>
-                        <Link className='menu-item' to="/addTraining">প্রশিক্ষণ</Link>
                         <Link className='menu-item' to="/addFieldDay">মাঠদিবস</Link>
-                        <Link className='menu-item' to="/addDistribution">বিতরণ</Link>
+                        <Link className='menu-item' to="/add-school">স্কুল</Link>                        
                         <Link className='menu-item' to="/add-dae-group-meeting">গ্রুপ সভা</Link>
+                        <Link className='menu-item' to="/dashboard/add-note">নোটস</Link>
+                     </>}
+
+                        {role === 'admin' && <>
+                        
+                        <Link className='menu-item' to="/addTraining">প্রশিক্ষণ</Link>
+                        <Link className='menu-item' to="/addDistribution">বিতরণ</Link>
                         <Link className='menu-item' to="/add-motivational-tour">ভ্রমণ</Link>
-                        <Link className='menu-item' to="/add-school">স্কুল</Link>
+                        </>
+                        }
                     </div>
                     <div className='mt-5 flex justify-center items-center flex-col'>
                         <GoPlusCircle
