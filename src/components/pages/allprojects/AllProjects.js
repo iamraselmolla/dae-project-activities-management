ad16c474e779e3d33feeebc0845a15faca1fd7cb
iamraselmolla <raselmolla6336@gmail.com>
2024-07-27 00:52:38 +0600

    filtered the project based on running and completed

--- src/components/pages/allprojects/AllProjects.js
+++ src/components/pages/allprojects/AllProjects.js
@@ -7,16 +7,18 @@ import { toBengaliNumber } from "bengali-number";
 import LoaderWithOutDynamicMessage from "../../shared/LoaderWithOutDynamicMessage";
 
 const AllProjects = () => {
-  const [projects, setAllProjects] = useState([]);
+  const [projects, setProjects] = useState([]);
+  const [filteredProjects, setFilteredProjects] = useState([]);
   const [loading, setLoading] = useState(true);
   const [projectType, setProjectType] = useState("all");
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const result = await findAllProjectsData();
         if (result?.status === 200) {
-          setAllProjects(result?.data?.data);
-          setLoading(false);
+          setProjects(result?.data?.data);
+          setFilteredProjects(result?.data?.data);
         }
       } catch (err) {
         toast.error("প্রকল্পের তথ্য সার্ভার থেকে আনতে অসুবিধা হচ্ছে।");
@@ -26,21 +28,16 @@ const AllProjects = () => {
     };
     fetchData();
   }, []);
-  const filterProjects = () => {
-    let filtered = projects;
-    if (demoType === "primary") {
-      filtered = filtered.filter((project) => !project.completed);
-    } else if (demoType === "final") {
-      filtered = filtered.filter((project) => project.completed);
-    }
 
-    return filtered;
-  };
   useEffect(() => {
-    const filtered = filterProjects();
-    setProjectType(filtered);
-  }, [projectType]);
-
+    let filtered = projects;
+    if (projectType === "running") {
+      filtered = filtered.filter((project) => !project.end);
+    } else if (projectType === "completed") {
+      filtered = filtered.filter((project) => project.end);
+    }
+    setFilteredProjects(filtered);
+  }, [projectType, projects]);
 
   return (
     <section className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
@@ -48,41 +45,39 @@ const AllProjects = () => {
         {!loading ? (
           <>
             <SectionTitle
-              title={`সকল প্রকল্প (${toBengaliNumber(projects?.length)})`}
+              title={`সকল প্রকল্প (${toBengaliNumber(filteredProjects?.length)})`}
             />
             <div role="tablist" className="tabs tabs-boxed">
               <a
                 role="tab"
                 onClick={() => setProjectType("all")}
-                className={`tab py-4 h-auto md:text-2xl text-lg font-bold  ${projectType === "all" && "text-white theme-bg"}`}
+                className={`tab py-4 h-auto md:text-2xl text-lg font-bold ${projectType === "all" && "text-white theme-bg"}`}
               >
                 সকল
               </a>
               <a
                 role="tab"
                 onClick={() => setProjectType("running")}
-                className={`tab py-4 h-auto md:text-2xl text-lg font-bold  ${projectType === "primary" && "text-white theme-bg"}`}
+                className={`tab py-4 h-auto md:text-2xl text-lg font-bold ${projectType === "running" && "text-white theme-bg"}`}
               >
                 চলমান
               </a>
               <a
                 role="tab"
                 onClick={() => setProjectType("completed")}
-                className={`tab py-4 h-auto md:text-2xl text-lg font-bold  ${projectType === "final" && "text-white theme-bg"}`}
+                className={`tab py-4 h-auto md:text-2xl text-lg font-bold ${projectType === "completed" && "text-white theme-bg"}`}
               >
                 সম্পন্ন
               </a>
             </div>
             <div className="grid grid-cols-1 mt-16 md:grid-cols-2 lg:grid-cols-3 gap-5">
-              {projects?.map((single, index) => (
+              {filteredProjects?.map((single) => (
                 <SingleProject key={single?.name?.details} single={single} />
               ))}
             </div>
           </>
         ) : (
-          <>
-            <LoaderWithOutDynamicMessage />
-          </>
+          <LoaderWithOutDynamicMessage />
         )}
       </div>
     </section>
