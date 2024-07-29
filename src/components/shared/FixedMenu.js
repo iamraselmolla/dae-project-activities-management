fe0993b2f310bdc8f653c912fff216afacc751ac
iamraselmolla <raselmolla6336@gmail.com>
2024-07-29 23:47:03 +0600

    added add a farmer module at fixed menu

--- src/components/shared/FixedMenu.js
+++ src/components/shared/FixedMenu.js
@@ -5,40 +5,42 @@ import { AuthContext } from "../AuthContext/AuthProvider";
 
 const FixedMenu = () => {
     const [open, setOpen] = useState(false);
-    const {role} = useContext(AuthContext)
+    const { role } = useContext(AuthContext)
 
 
 
     return (
         <>
-             <div className="fixed flex justify-center flex-col bottom-5 right-5 items-center">
-                    <div className={`menu-drawer ${open ? 'open' : ''}`}>
-                     {role === 'user' &&   <>
+            <div className="fixed flex justify-center flex-col bottom-5 right-5 items-center">
+                <div className={`menu-drawer ${open ? 'open' : ''}`}>
+                    {role === 'user' && <>
                         <Link className='menu-item' to="/addDemo">প্রদর্শনী</Link>
                         <Link className='menu-item' to="/addFieldDay">মাঠদিবস</Link>
-                        <Link className='menu-item' to="/add-school">স্কুল</Link>                        
+                        <Link className='menu-item' to="/add-school">স্কুল</Link>
                         <Link className='menu-item' to="/add-dae-group-meeting">গ্রুপ সভা</Link>
                         <Link className='menu-item' to="/dashboard/add-note">নোটস</Link>
-                     </>}
+                        <Link className='menu-item' to="/add-farmer">কৃষক</Link>
+                    </>}
 
-                        {role === 'admin' && <>
-                        
+                    {role === 'admin' && <>
+
                         <Link className='menu-item' to="/addTraining">প্রশিক্ষণ</Link>
                         <Link className='menu-item' to="/addDistribution">বিতরণ</Link>
                         <Link className='menu-item' to="/add-motivational-tour">ভ্রমণ</Link>
-                        </>
-                        }
-                    </div>
-                    <div className='mt-5 flex justify-center items-center flex-col'>
-                        <GoPlusCircle
-                            onClick={() => setOpen(!open)}
-                            cursor={'pointer'}
-                            color="green"
-                            size={60}
-                            className={`menu-icon mr-5 ${open ? 'open' : ''}`}
-                        />
-                    </div>
+                    </>
+                    }
+
                 </div>
+                <div className='mt-5 flex justify-center items-center flex-col'>
+                    <GoPlusCircle
+                        onClick={() => setOpen(!open)}
+                        cursor={'pointer'}
+                        color="green"
+                        size={60}
+                        className={`menu-icon mr-5 ${open ? 'open' : ''}`}
+                    />
+                </div>
+            </div>
         </>
     )
 }
