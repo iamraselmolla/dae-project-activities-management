70f41952515025ec02657eef34db88d9366ef763
iamraselmolla <raselmolla6336@gmail.com>
2024-07-31 12:33:17 +0600

    added section title for noices

--- src/components/pages/notices/Notices.js
+++ src/components/pages/notices/Notices.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import SectionTitle from '../../shared/SectionTitle';
 
 const Notices = () => {
     return (
-        <div>
-
-        </div>
+        <section className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+            <div className="text-right font-extrabold col-span-1">
+                <SectionTitle
+                    title={`সকল নোটিশ`}
+                />
+            </div>
+        </section>
     );
 };
 
